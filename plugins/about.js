4fcaf9ed8847ffece575873b13d20298931b6a03
BLACKVENOM499 <blackvenomx499@gmail.com>
2025-05-21 21:23:50 +0530

    Update about.js

--- plugins/about.js
+++ plugins/about.js
@@ -1,8 +1,7 @@
 const config = require('../config')
 const {cmd , commands} = require('../command')
 cmd({
-    pattern: "owner",
-    alias: ["akindu","owner"], 
+    pattern: "about",
     react: "👑",
     desc: "get owner dec",
     category: "main",
@@ -10,37 +9,17 @@ cmd({
 },
 async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
 try{
-let about = `
-*╭━━〔 AKINDU MD 〕━━┈⊷*
+let madeMenu = `*👋 ʜᴇʟʟᴏ ${pushname}*
+ɪ'ᴍ ᴀᴋɪɴᴅᴜ ᴅɪᴍᴀɴꜱʜᴀ.
+ᴏᴡɴᴇʀ ᴏꜰ ᴀᴋɪɴᴅᴜ ᴍᴅ ᴡʜᴀᴛꜱᴀᴘᴘ ʙᴏᴛ.
+ɪ'ᴍ ꜰʀᴏᴍ ꜱʀɪ ʟᴀɴᴋᴀ.
+16 ʏᴇᴀʀꜱ ᴏʟᴅ.
 
-*👋 HELLO ${pushname}*
+> ᴀᴋɪɴᴅᴜ ᴍᴅ`
 
-*╰──────────────┈⊷*
-*╭━━━〔 MY ABOUT 〕━━━┈⊷*
-*┃★╭──────────────*
-*┃★│* *ᴡᴇʟᴄᴏᴍᴇ ɪᴛs ᴀᴋɪɴᴅᴜ ᴍᴅ*
-*┃★│* *ᴄʀᴇᴀᴛᴇʀ : ᴀᴋɪɴᴅᴜ*
-*┃★│* *ʀᴇᴀʟ ɴᴀᴍᴇ : ᴀᴋɪɴᴅᴜ ᴅɪᴍᴀɴꜱʜᴀ*
-*┃★│* *ᴘᴜʙʟɪᴄ ɴᴀᴍᴇ : ᴀᴋɪ>>🖤*
-*┃★│* *ᴀɢᴇ : 16 ʏᴇᴀʀ*
-*┃★│* *ᴄɪᴛʏ : ᴍᴀᴛᴀʀᴀ*
-*┃★│* *ᴀ sɪᴍᴘʟᴇ ᴡʜᴀᴛsᴀᴘᴘ ᴅᴇᴠᴇʟᴘᴏʀ*
-    ╰──────────────*
-> AKINDU MD`
+await conn.sendMessage(from,{image:{url:config.ALIVE_IMG},caption:madeMenu},{quoted:mek})
 
-await conn.sendMessage(from,{image:{url:`https://i.ibb.co/nMjszVMd/SulaMd.jpg`},caption:about,
-                             contextInfo: {
-    mentionedJid: [m.sender],
-    forwardingScore: 999,
-    isForwarded: true,
-    forwardedNewsletterMessageInfo: {
-      newsletterJid: '120363400898693892@newsletter',
-      newsletterName: 'AKINDU MD',
-      serverMessageId: 999
-    }
-  }
-}, { quoted: mek });
-} catch (e) {
+}catch(e){
 console.log(e)
 reply(`${e}`)
 }
