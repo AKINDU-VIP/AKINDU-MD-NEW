9cd5613466f1cbd3acf83e828c0d3ae9bf9bc1cd
BLACKVENOM499 <blackvenomx499@gmail.com>
2025-05-22 06:30:38 +0530

    Update alive.js

--- plugins/alive.js
+++ plugins/alive.js
@@ -3,18 +3,20 @@ const {cmd , commands} = require('../command')
 cmd({
     pattern: "alive",
     react: "👋",
-    desc: "Check bot online or no.",
+    desc: "check bot alive",
     category: "main",
     filename: __filename
 },
 async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
 try{
-return await conn.sendMessage(from,{image: {url: config.ALIVE_IMG},caption: config.ALIVE_MSG},{quoted: mek})
+let madeMenu = `*👋 ʜᴇʟʟᴏ ${pushname}* \n
+❒ *ᴀᴋɪɴᴅᴜ ᴍᴅ ɴᴏᴡ ᴀʟɪᴠᴇ 😇* \n *ʜᴏᴡ ᴄᴀɴ ɪ ʜᴇʟᴘ ʏᴏᴜ ᴛᴏᴀᴅʏ* \n *ɪꜰ ʏᴏᴜ ᴡᴀɴᴛ ᴛᴏ ɢᴇᴛ ʙᴏᴛ ᴍᴇɴᴜ ᴛʏᴘᴇ .ᴍᴇɴᴜ 📜*
+> ᴀᴋɪɴᴅᴜ ᴍᴅ`
+
+await conn.sendMessage(from,{image:{url:config.ALIVE_IMG},caption:madeMenu},{quoted:mek})
+
 }catch(e){
 console.log(e)
 reply(`${e}`)
 }
 })
-
-
-
